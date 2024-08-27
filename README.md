9afda3449039675c298be922b8590bac88ac3260
Shreeba Firdose <fshareeba@gmail.com>
2024-08-27 23:34:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Quiz App
+
+The Quiz App is a web-based application that allows users to create, manage, and take quizzes. It features a user-friendly interface for quiz creation, a responsive design, and real-time functionality to enhance the quiz-taking experience.
+
+# Installation
+
+cd backend
+npm install
+
+cd ../frontend
+npm install
+
+
+# Run the Application
+
+Start the backend server:
+
+cd backend
+npm start
+
+cd ../frontend
+npm start
+
---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import router from "./routes/routes.js";
+import mongoose from "mongoose";
+import { fileURLToPath } from 'url';
+import { dirname } from "path";
+import cors from "cors";
+
+const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const DB_PWD = process.env.DB_PWD || "";
+
+
+const server = express();
+
+// middlewares
+server.use(cors());
+server.use(express.json());
+server.use("/api", router); // router middleware should be placed at the last
+
+
+const URI = "mongodb+srv://Shareeba:sharufirdz@cluster0.jl7kc.mongodb.net/quiz";
+mongoose.connect(URI, {dbName: "main"}).then(() => console.log("db connected sucessfully!")).catch((e) => console.log(e));
+
+
+server.listen(PORT, () => console.log("server started successfully on port:", PORT));
